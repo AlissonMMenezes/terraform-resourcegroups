7dcdc3fef4be95ed9a8e86aa1d90de7154dbe55d
Alisson Menezes <Alisson.itops@gmail.com>
2022-03-05 15:20:00 +0100

    uploading initial module

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Environment name"
+  default     = "West Europe"
+}
